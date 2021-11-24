a0b366d150af93fe95a15b5beb560f37803069c9
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-24 11:37:38 -0500

    Creado el controlador en un archivo js aparte que se encargara de la logica con una entidad de prueba 'Profesor'

---
+++ js/aplicacion.js
@@ -0,0 +1,10 @@
+const aplicacion = angular.module("universidadApp", []);
+
+aplicacion.controller("profesorCtrl", function () {
+  this.profesor = {
+    nombre: "Carlos Ernesto Díaz Basante",
+    bio: "Hola, un gusto en conocerte, espero que podamos llevarnos bien y aprender mucho sobre Angularjs",
+    edad: 23,
+    foto: "img/carlosd.jpg",
+  };
+});
