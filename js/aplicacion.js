dfb279c39faf814101d3f2206b403e894ba39f47
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-24 15:10:35 -0500

    Ejercio para crear productos y listarlos localmente, Se creo el form de prodcutos

--- js/aplicacion.js
+++ js/aplicacion.js
@@ -1,10 +1,5 @@
 const aplicacion = angular.module("universidadApp", []);
 
-aplicacion.controller("profesorCtrl", function () {
-  this.profesor = {
-    nombre: "Carlos Ernesto Díaz Basante",
-    bio: "Hola, un gusto en conocerte, espero que podamos llevarnos bien y aprender mucho sobre Angularjs",
-    edad: 23,
-    foto: "../img/carlosd.jpg",
-  };
+aplicacion.controller("productoCtrl", function () {
+  this.profesor = [];
 });
